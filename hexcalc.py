4b7e32551bbcf818a09cab2b7455eefe3870a46f
tsurrdurr <tsurrdurr@users.noreply.github.com>
2017-05-06 14:41:00 +0300

    created console script

---
+++ hexcalc.py
@@ -0,0 +1,29 @@
+def main():
+    while(True):
+        print(">>")
+        text = str(input("Enter cmd: ")).upper()
+        result = get_checksum(text).upper()
+        if(result[-2:] == text[-2:]):
+            print("Checksum correct")
+        else:
+            print("Line with new checksum: " + text[:-2] + result[-2:])
+
+def get_checksum(text):
+    byte_count = int(text[1:3], 16)
+
+    address = str(text[3:9 + byte_count*2])
+    i = 0
+    sum = byte_count
+    while(i < len(address)):
+        byte_hex = address[i:i+2]
+        byte_value = int(byte_hex, 16)
+        sum += byte_value
+        i+=2
+    result = compliment(sum)
+    return hex(result)
+
+def compliment(value):
+    return (value ^ 0xFFFF) + 1
+
+
+if  __name__ =='__main__':main()
